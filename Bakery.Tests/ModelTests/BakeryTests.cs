b97f70e5e74bc3875c54bf7f7a05657d7613f480
Ellie Lambert <ebezjian@gmail.com>
2020-12-11 16:14:40 -0500

    purposful fail on fourth test

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -34,7 +34,7 @@ namespace Bakery.Tests
       public void GetPastryPrice_ReturnsPatryPrice_Int()
       {
         int testPastryPrice = 2;
-        Pastry newPastry = new Pastry(2);
+        Pastry newPastry = new Pastry(3);
         int results = newPastry.PastryPrice;
         Assert.AreEqual(testPastryPrice, results);
       }
